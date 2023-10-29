01f10c85a470c208d31c4966080c091fbd098b16
Bonace Patrick <patrickruboneza@gmail.com>
2023-10-29 17:17:40 +0200

    install Chackra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
+import { Button } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-
-    <h1>Hello react</h1>
-      
+      <Button colorScheme="blue">Button</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
