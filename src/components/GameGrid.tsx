cf87c5756ec506e963841cc0f1c00d071346ab64
Bonace Patrick <patrickruboneza@gmail.com>
2023-11-04 21:26:08 +0200

    Refactor: Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,10 +14,9 @@ const GameGrid = ({gameQuery}:Props) => {
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
+  if (errors) return <Text>{errors}</Text>
 
   return (
-    <>
-      {errors && <Text>{errors}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={"0.9rem"}
@@ -36,7 +35,6 @@ const GameGrid = ({gameQuery}:Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
